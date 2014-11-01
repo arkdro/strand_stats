41e524edf15399aad0fdfc522482895d9c7c312a
Arkadiy Drovosekov <arkdro@gmail.com>
2014-11-01 20:10:29 +0300

    strand_stats.skew/skew

--- src/strand_stats/skew.clj
+++ src/strand_stats/skew.clj
@@ -2,8 +2,17 @@
   ;; (:require bia_utils.util)
   )
 
-(defn skew [start stop text]
-  (let [
-        ]
-    nil))
+(defn skew-aux [lst cnt acc-lst]
+  (if (empty? lst) (reverse acc-lst)
+      (case (first lst)
+        \C (recur (rest lst) (dec cnt) (cons (dec cnt) acc-lst))
+        \G (recur (rest lst) (inc cnt) (cons (inc cnt) acc-lst))
+        \A (recur (rest lst) cnt (cons cnt acc-lst))
+        \T (recur (rest lst) cnt (cons cnt acc-lst))
+        )
+      )
+  )
+
+(defn skew [text]
+  (skew-aux (seq text) 0 '(0)))
 
