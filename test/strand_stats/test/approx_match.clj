277e9d24d7d6c9182350faa3d3d741b3c528a8e4
Arkadiy Drovosekov <arkdro@gmail.com>
2014-11-02 19:59:55 +0300

    add test for calc-combinations

--- test/strand_stats/test/approx_match.clj
+++ test/strand_stats/test/approx_match.clj
@@ -58,5 +58,7 @@
 (deftest calc-combinations-test
   (is (= [[0] [1] [2] [3] [0 1] [0 2] [0 3] [1 2] [1 3] [2 3]]
          (strand_stats.approx_match/calc-combinations 2 "CAGA")))
+  (is (= [[0] [1] [2] [0 1] [0 2] [1 2]]
+         (strand_stats.approx_match/calc-combinations 2 "CGA")))
   )
 
