ba2abb1acb9363b4eb3f4e207932e7439dbb278f
Arkadiy Drovosekov <arkdro@gmail.com>
2014-11-02 18:20:07 +0300

    add tests

--- test/strand_stats/test/approx_match.clj
+++ test/strand_stats/test/approx_match.clj
@@ -12,3 +12,28 @@
          (strand_stats.approx_match/replace-one-char (vec "CGGAC") 4 \A)))
   )
 
+(deftest fill-one-position-test
+  (is (= '("CAGAC"
+           "CCGAC"
+           "CGGAC"
+           "CTGAC")
+         (strand_stats.approx_match/fill-one-position 1 (vec "CGGAC"))))
+  (is (= '("CGGAA"
+           "CGGAC"
+           "CGGAG"
+           "CGGAT")
+         (strand_stats.approx_match/fill-one-position 4 (vec "CGGAC"))))
+  )
+
+(deftest build-one-approx-pattern-test
+  (is (= ["CAGAC" "CCGAC" "CGGAC" "CTGAC"
+          "CGGAA" "CGGAC" "CGGAG" "CGGAT"]
+         (strand_stats.approx_match/build-one-approx-pattern
+          [1 4] (vec "CGGAC"))))
+  )
+
+(deftest calc-combinations-test
+  (is (= [[0] [1] [2] [3] [0 1] [0 2] [0 3] [1 2] [1 3] [2 3]]
+         (strand_stats.approx_match/calc-combinations 2 "CAGA")))
+  )
+
